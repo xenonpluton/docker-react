3ecdf2adec83baec0ee6962d7ac7e2bfd5edf3d5
xenonpluton <60020612+xenonpluton@users.noreply.github.com>
2020-04-12 22:06:59 +0100

    added feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi REDA IS THE BEST !
+          Hi FEATURE IS THE BEST !
         </p>
         <a
           className="App-link"
